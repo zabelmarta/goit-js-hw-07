e0bff0e68ee3467e13cee82060b33628ceeb7ad7
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-01-19 22:06:36 +0100

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+//export const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  };
+
+
+  
+const galleryBox = document.querySelector(".gallery");
+
+const galleryList = document.createElement("ul");
+const galleryItem = document.createElement("li");
+
+
+
+
+
 console.log(galleryItems);
