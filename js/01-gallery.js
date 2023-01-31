b70e353ad33be15883bb932c57e2ab0c11938509
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-01-31 18:21:32 +0100

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-//export const galleryItems = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  };
+const gallery = document.querySelector('.gallery');
+const createGallery = createCardImg(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+gallery.addEventListener("click", onGalleryClick);
 
 
-  
-const galleryBox = document.querySelector(".gallery");
+function createCardImg(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`;
+    })
+    .join("");
+};
 
-const galleryList = document.createElement("");
 
-galleryItems.forEach(el)=> {
-  const galleryItem = document.createElement("img");
-
-  galleryList.appendChild(galleryItem);
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="1280px">`,
+    {
+      onShow: (instance) => {
+        gallery.addEventListener('keydown', onGalleryClickEsc);
+      },
+      onClose: (instance) => {
+        gallery.removeEventListener("keydown", onGalleryClickEsc);
+      },
+    }
+  )
+    instance.show();
+    
+    function onGalleryClickEsc(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
 }
 
-
-
-
-
-
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const createGallery = createCardsImg(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+
+function createCardsImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"></a>`;
+    })
+    .join("");
+}
+
+const ligthBox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+  close: false,
+});
